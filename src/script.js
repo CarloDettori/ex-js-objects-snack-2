ed99a2afdaee76e9007baa6bd785ccfcf7e0019a
CarloDettori <dettoric07@gmail.com>
2025-03-16 16:42:57 +0100

    scafoldin

---
+++ src/script.js
@@ -0,0 +1,57 @@
+//SNACK1_______________________________________________
+// 1 Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// 2 Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+
+
+
+
+
+//SNACK2_______________________________________________
+//P.S.: Ricordati che gli Array, come gli oggetti, sono dei Reference Type (Tipi di Riferimento)!
+// 1 Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// 2 Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+
+
+
+
+
+//SNACK3_______________________________________________
+// 1 Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+
+
+
+
+
+//SNACK4_______________________________________________
+// 1 Qual è il metodo migliore per clonare l’oggetto chef, e perché?
+// 2 Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?
+
+
+
+
+
+
+//SNACK5_______________________________________________
+// 1 Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
+// 2 Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
+
+
+
+
+
+
+//SNACK6_______________________________________________
+// 1 Qual è il metodo migliore per clonare l’oggetto chef, e perché?
+//
+
+
+
+
+
+
+//SNACK7_______________________________________________
+// Crea una funzione che permette la copia profonda (deep copy) di un oggetto, che copia anche i suoi metodi (proprietà che contengono funzioni). Usa l’oggetto di Code Question 6 come test.
+// Serve usare una funzione ricorsiva! (fai un po’ di ricerca).
