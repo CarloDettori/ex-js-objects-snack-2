26aeee5866114a3f5744054348c91338c3c6efa7
CarloDettori <dettoric07@gmail.com>
2025-03-17 18:22:20 +0100

    snack 7 + alcune aggiunte alle risposte

--- src/script.js
+++ src/script.js
@@ -2,10 +2,8 @@
 // 1 Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 // 2 Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 
-// 1 console.log(hamburger.name) -> Double Cheese Burger; console.log(secondBurger.name) -> Double Cheese Burger (in realtà non viene stampato nulla)
-// 2 1
-
-
+// 1 console.log(hamburger.name) -> Double Cheese Burger; console.log(secondBurger.name) -> Double Cheese Burger viene creata una referenza allo stesso oggetto per entrambe le variabili
+// 2 1 perchè vien crata solo una nuova referenza, non un nuovo oggetto
 
 
 
@@ -15,8 +13,8 @@
 // 1 Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 // 2 Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 
-// 1 console.log(hamburger.ingredients[0]) -> ["Cheese", "Meat", "Bread", "Tomato"]; console.log(secondBurger.ingredients[0]) -> array
-// 2 2
+// 1 console.log(hamburger.ingredients[0]) -> Salad; console.log(secondBurger.ingredients[0]) -> Salad (riferito all'originale) perche lo spreading crea una copia superficiale dell'oggetto originale ma non di altri oggetti annidati quindi "Salad" viene assegnatao all'oggetto ingredienti che vale per entrambe le copie
+// 2 3 in totale perche la copia creata con spread è pur sempre una copia nonostante sia superficiale, tuttavia l'array nell primo oggetto
 
 
 
@@ -24,7 +22,7 @@
 //SNACK3_______________________________________________
 // 1 Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 
-// 1 2
+// 1 6 perche oltre alle copie bisogna tener conto anche degli oggetti annidati
 
 
 
@@ -33,8 +31,8 @@
 // 1 Qual è il metodo migliore per clonare l’oggetto chef, e perché?
 // 2 Qual è il metodo migliore per clonare l’oggetto restaurant, e perché?
 
-// 1 spread, perche copia le funzioni
-// 2 structuredClone(), perche copia oggetti annidati anche quando sono complessi
+// 1 spread, perche non ci sono oggetti annidati e lo spread copia anche le funzioni
+// 2 structuredClone(), perche copia oggetti annidati anche quando sono complessi e non ci sono funzioni annidate
 
 
 
@@ -43,11 +41,11 @@
 // 1 Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 // 2 Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 
-// 1 console.log(hamburger.maker.name) -> Anonymous Chef;
+// 1 console.log(hamburger.maker.name) -> Chef Hyur;
 //   console.log(secondBurger.maker.name) -> Chef Hyur;
-//   console.log(hamburger.maker.restaurant.name) -> Hyur's Burgers;
+//   console.log(hamburger.maker.restaurant.name) -> Hyur's II;
 //   console.log(secondBurger.maker.restaurant.name) -> Hyur's II;
-// 2 2
+// 2 5 in totale 
 
 
 
@@ -57,7 +55,7 @@
 //SNACK6_______________________________________________
 // 1 Qual è il metodo migliore per clonare l’oggetto chef, e perché?
 
-// 1 un mix tra spread e structured clone perche chef è un oggetto complesso con vari gradi di profondità ma presenta anche una funzione annidata
+// 1 la scelta migliore sarebbe un mix tra spread e structuredClone, structuredClone sarà necessario perche chef è un oggetto complesso con vari gradi di profondità perciò avremo bisogno di una copia profonta ma. Tuttavia presenta anche una funzione annidata, quindi sarà necessario anche uno spread perche altrimenti non possiamo copiare la funzione. la scelta definitiva sarebbe utilizzare la libreria esterna lodash che permette una copia totale dell'oggetto preso in questione
 
 
 
@@ -66,3 +64,44 @@
 //SNACK7_______________________________________________
 // Crea una funzione che permette la copia profonda (deep copy) di un oggetto, che copia anche i suoi metodi (proprietà che contengono funzioni). Usa l’oggetto di Code Question 6 come test.
 // Serve usare una funzione ricorsiva! (fai un po’ di ricerca).
+
+const chef = {
+    name: "Chef Hyur",
+    age: 29,
+    makeBurger: (num = 1) => {
+        console.log(`Ecco ${num} hamburger per te!`);
+    },
+    restaurant: {
+        name: "Hyur's Burgers",
+        welcomeClient: () => {
+            console.log("Benvenuto!");
+        },
+        address: {
+            street: 'Main Street',
+            number: 123,
+            showAddress: () => {
+                console.log("Main Street 123");
+            }
+        },
+        isOpen: true,
+    }
+}
+
+function cloner(object) {
+    if (typeof object !== 'object') {
+        return object
+    }
+    const copia = {}
+    for (const key in object) {
+        const valore = object[key]
+        if (typeof valore !== "object") {
+            copia[key] = valore;
+        } else {
+            copia[key] = cloner(valore)
+        }
+    }
+    return copia
+}
+
+const chefCopy = cloner(chef)
+console.log(chefCopy)
